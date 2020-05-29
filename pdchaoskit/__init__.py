b1b648be73ea1168d38dc8716d0948d719c20f03
proofdock-support <support@proofdock.io>
2020-05-29 20:38:52 +0000

    Bump version: 0.0.4-dev0 → 0.0.4-dev1 [skip ci]

--- pdchaoskit/__init__.py
+++ pdchaoskit/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.0.4-dev0'
+__version__ = '0.0.4-dev1'
 
 from pdchaoskit.vcs import (GitInformationStrategy, PerforceInformationStrategy,
                             VcsInformationStrategy)

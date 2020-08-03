f469aecfc54d72adf77b15f03c0820b9a3615f41
proofdock-support <support@proofdock.io>
2020-08-03 20:10:09 +0000

    Bump version: 1.0.3-dev0 → 1.0.3 [skip ci]

--- pdchaoskit/__init__.py
+++ pdchaoskit/__init__.py
@@ -5,7 +5,7 @@ from chaoslib.discovery.discover import (discover_probes,
 from chaoslib.types import DiscoveredActivities, Discovery
 from logzero import logger
 
-__version__ = '1.0.3-dev0'
+__version__ = '1.0.3'
 
 
 def discover(discover_system: bool = True) -> Discovery:
